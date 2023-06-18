306718712d58ed151039ffe5d09dfceca9fe95a3
Alex Suraci <suraci.alex@gmail.com>
2023-06-18 10:31:30 -0400

    Revert "finish swapping out DNS with static host config"
    Trying to bring back Git/HTTP service usage, since Projects uses it, and
    it's nice to have a fully integrated API.
    
    This reverts commit 9456f3461741a8c98e07ff97f104338ffbac3e10.
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- core/integration/services_test.go
+++ core/integration/services_test.go
@@ -6,8 +6,9 @@
 // just-in-time for a File or Directory accessed from it.
 //
 // So, in order to actually test that services convey to/from a Directory or
-// File and are started just-in-time wherever they end up, be sure to specify
-// lazy: true to Container.File/Directory.
+// File and are started just-in-time wherever they end up, a few of these tests
+// instead use the Git and HTTP Dagger APIs since they will directly yield a
+// Directory or File without eager evaluation.
 
 package core
 
@@ -76,8 +77,6 @@ func TestServiceHostnameEndpoint(t *testing.T) {
 	defer c.Close()
 
 	t.Run("hostname is independent of exposed ports", func(t *testing.T) {
-		t.Skip("no longer the case; does it matter?")
-
 		a, err := c.Container().
 			From("python").
 			WithExposedPort(8000).
@@ -527,11 +526,19 @@ func TestContainerExecServicesChained(t *testing.T) {
 	srv, _ := httpService(ctx, t, c, "0\n")
 
 	for i := 1; i < 10; i++ {
-		index := wget(ctx, t, c, srv)
+		httpURL, err := srv.Endpoint(ctx, dagger.ServiceEndpointOpts{
+			Scheme: "http",
+		})
+		require.NoError(t, err)
 
 		srv = c.Container().
 			From("python").
-			WithFile("/srv/www/index.html", index).
+			WithFile(
+				"/srv/www/index.html",
+				c.HTTP(httpURL, dagger.HTTPOpts{
+					ExperimentalServiceHost: srv,
+				}),
+			).
 			WithExec([]string{"sh", "-c", "echo $0 >> /srv/www/index.html", strconv.Itoa(i)}).
 			WithWorkdir("/srv/www").
 			WithExposedPort(8000).
@@ -557,8 +564,6 @@ func TestContainerBuildService(t *testing.T) {
 	defer c.Close()
 
 	t.Run("building with service dependency", func(t *testing.T) {
-		t.Skip("this no longer works, and it's kind of weird that it ever did")
-
 		content := identity.NewID()
 		srv, httpURL := httpService(ctx, t, c, content)
 
@@ -579,8 +584,6 @@ CMD cat index.html
 	})
 
 	t.Run("building a directory that depends on a service (Container.Build)", func(t *testing.T) {
-		t.Skip("this no longer works, and it's kind of weird that it ever did")
-
 		content := identity.NewID()
 		srv, httpURL := httpService(ctx, t, c, content)
 
@@ -592,8 +595,11 @@ RUN wget `+httpURL+`
 CMD cat index.html
 `)
 
-		gitDaemon, _ := gitService(ctx, t, c, src)
-		gitDir := clone(ctx, t, c, gitDaemon)
+		gitDaemon, repoURL := gitService(ctx, t, c, src)
+
+		gitDir := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+			Branch("main").
+			Tree()
 
 		fileContent, err := c.Container().
 			WithServiceBinding("www", srv).
@@ -604,8 +610,6 @@ CMD cat index.html
 	})
 
 	t.Run("building a directory that depends on a service (Directory.DockerBuild)", func(t *testing.T) {
-		t.Skip("this no longer works, and it's kind of weird that it ever did")
-
 		content := identity.NewID()
 		srv, httpURL := httpService(ctx, t, c, content)
 
@@ -617,8 +621,11 @@ RUN wget `+httpURL+`
 CMD cat index.html
 `)
 
-		gitDaemon, _ := gitService(ctx, t, c, src)
-		gitDir := clone(ctx, t, c, gitDaemon)
+		gitDaemon, repoURL := gitService(ctx, t, c, src)
+
+		gitDir := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+			Branch("main").
+			Tree()
 
 		fileContent, err := gitDir.
 			DockerBuild().
@@ -742,7 +749,7 @@ func TestContainerWithRootFSServices(t *testing.T) {
 	content := identity.NewID()
 	srv, url := httpService(ctx, t, c, content)
 
-	gitDaemon, _ := gitService(ctx, t, c,
+	gitDaemon, repoURL := gitService(ctx, t, c,
 		// this little maneuver commits the entire rootfs into a git repo
 		c.Container().
 			From("alpine:3.16.2").
@@ -755,7 +762,9 @@ func TestContainerWithRootFSServices(t *testing.T) {
 			// exists, and is always a directory.
 			Rootfs())
 
-	gitDir := clone(ctx, t, c, gitDaemon)
+	gitDir := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree()
 
 	fileContent, err := c.Container().
 		WithRootfs(gitDir).
@@ -844,11 +853,15 @@ func TestContainerWithServiceFileDirectory(t *testing.T) {
 	defer c.Close()
 
 	response := identity.NewID()
-	srv, _ := httpService(ctx, t, c, response)
-	httpFile := wget(ctx, t, c, srv)
+	srv, httpURL := httpService(ctx, t, c, response)
+	httpFile := c.HTTP(httpURL, dagger.HTTPOpts{
+		ExperimentalServiceHost: srv,
+	})
 
-	gitDaemon, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", response))
-	gitDir := clone(ctx, t, c, gitDaemon)
+	gitDaemon, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", response))
+	gitDir := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree()
 
 	t.Run("mounting", func(t *testing.T) {
 		useBoth := c.Container().
@@ -891,11 +904,14 @@ func TestDirectoryServiceEntries(t *testing.T) {
 
 	content := identity.NewID()
 
-	gitDaemon, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
+	gitDaemon, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
 
-	entries, err := clone(ctx, t, c, gitDaemon).Entries(ctx)
+	entries, err := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree().
+		Entries(ctx)
 	require.NoError(t, err)
-	require.ElementsMatch(t, []string{".git", "README.md"}, entries)
+	require.Equal(t, []string{"README.md"}, entries)
 }
 
 func TestDirectoryServiceTimestamp(t *testing.T) {
@@ -907,10 +923,12 @@ func TestDirectoryServiceTimestamp(t *testing.T) {
 	defer c.Close()
 
 	content := identity.NewID()
-	gitDaemon, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
+	gitDaemon, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
 
 	ts := time.Date(1991, 6, 3, 0, 0, 0, 0, time.UTC)
-	stamped := clone(ctx, t, c, gitDaemon).
+	stamped := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree().
 		WithTimestamps(int(ts.Unix()))
 
 	stdout, err := c.Container().From("alpine:3.16.2").
@@ -931,17 +949,17 @@ func TestDirectoryWithDirectoryFileServices(t *testing.T) {
 
 	content := identity.NewID()
 
-	gitSrv, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
+	gitSrv, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
 
-	httpSrv, _ := httpService(ctx, t, c, content)
+	httpSrv, httpURL := httpService(ctx, t, c, content)
 
 	useBoth := c.Directory().
-		WithDirectory("/repo", clone(ctx, t, c, gitSrv)).
-		WithFile("/index.html", wget(ctx, t, c, httpSrv))
+		WithDirectory("/repo", c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitSrv}).Branch("main").Tree()).
+		WithFile("/index.html", c.HTTP(httpURL, dagger.HTTPOpts{ExperimentalServiceHost: httpSrv}))
 
 	entries, err := useBoth.Directory("/repo").Entries(ctx)
 	require.NoError(t, err)
-	require.ElementsMatch(t, []string{".git", "README.md"}, entries)
+	require.Equal(t, []string{"README.md"}, entries)
 
 	fileContent, err := useBoth.File("/index.html").Contents(ctx)
 	require.NoError(t, err)
@@ -958,11 +976,13 @@ func TestDirectoryServiceExport(t *testing.T) {
 
 	content := identity.NewID()
 
-	gitDaemon, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
+	gitDaemon, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
 
 	dest := t.TempDir()
 
-	ok, err := clone(ctx, t, c, gitDaemon).
+	ok, err := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree().
 		Export(ctx, dest)
 	require.NoError(t, err)
 	require.True(t, ok)
@@ -982,9 +1002,11 @@ func TestFileServiceContents(t *testing.T) {
 
 	content := identity.NewID()
 
-	gitDaemon, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
+	gitDaemon, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
 
-	fileContent, err := clone(ctx, t, c, gitDaemon).
+	fileContent, err := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree().
 		File("README.md").
 		Contents(ctx)
 	require.NoError(t, err)
@@ -1001,12 +1023,14 @@ func TestFileServiceExport(t *testing.T) {
 
 	content := identity.NewID()
 
-	gitDaemon, _ := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
+	gitDaemon, repoURL := gitService(ctx, t, c, c.Directory().WithNewFile("README.md", content))
 
 	dest := t.TempDir()
 	filePath := filepath.Join(dest, "README.md")
 
-	ok, err := clone(ctx, t, c, gitDaemon).
+	ok, err := c.Git(repoURL, dagger.GitOpts{ExperimentalServiceHost: gitDaemon}).
+		Branch("main").
+		Tree().
 		File("README.md").
 		Export(ctx, filePath)
 	require.NoError(t, err)
@@ -1027,10 +1051,10 @@ func TestFileServiceTimestamp(t *testing.T) {
 
 	content := identity.NewID()
 
-	httpSrv, _ := httpService(ctx, t, c, content)
+	httpSrv, httpURL := httpService(ctx, t, c, content)
 
 	ts := time.Date(1991, 6, 3, 0, 0, 0, 0, time.UTC)
-	stamped := wget(ctx, t, c, httpSrv).
+	stamped := c.HTTP(httpURL, dagger.HTTPOpts{ExperimentalServiceHost: httpSrv}).
 		WithTimestamps(int(ts.Unix()))
 
 	stdout, err := c.Container().From("alpine:3.16.2").
@@ -1051,10 +1075,12 @@ func TestFileServiceSecret(t *testing.T) {
 
 	content := identity.NewID()
 
-	httpSrv, _ := httpService(ctx, t, c, content)
+	httpSrv, httpURL := httpService(ctx, t, c, content)
 
 	//nolint:staticcheck // SA1019 We want to test this API while we support it.
-	secret := wget(ctx, t, c, httpSrv).Secret()
+	secret := c.HTTP(httpURL, dagger.HTTPOpts{
+		ExperimentalServiceHost: httpSrv,
+	}).Secret()
 
 	t.Run("secret env", func(t *testing.T) {
 		exitCode, err := c.Container().
@@ -1145,30 +1171,4 @@ git daemon --verbose --export-all --base-path=/root/srv
 	repoURL := fmt.Sprintf("git://%s/repo.git", gitHost)
 
 	return gitDaemon, repoURL
-}
-
-func wget(ctx context.Context, t *testing.T, c *dagger.Client, svc *dagger.Service) *dagger.File {
-	url, err := svc.Endpoint(ctx, dagger.ServiceEndpointOpts{
-		Scheme: "http",
-	})
-	require.NoError(t, err)
-
-	return c.Container().
-		From("alpine:3.16.2").
-		WithServiceBinding("www", svc).
-		WithExec([]string{"wget", url}).
-		File("index.html", dagger.ContainerFileOpts{
-			Lazy: true,
-		})
-}
-
-func clone(ctx context.Context, t *testing.T, c *dagger.Client, svc *dagger.Service) *dagger.Directory {
-	return c.Container().
-		From("alpine:3.16.2").
-		WithExec([]string{"apk", "add", "git"}).
-		WithServiceBinding("daemon", svc).
-		WithExec([]string{"git", "clone", "git://daemon/repo.git"}).
-		Directory("repo", dagger.ContainerDirectoryOpts{
-			Lazy: true,
-		})
 }
--- sdk/nodejs/api/client.gen.ts
+++ sdk/nodejs/api/client.gen.ts
@@ -115,13 +115,6 @@ export type ContainerBuildOpts = {
   secrets?: Secret[]
 }
 
-export type ContainerDirectoryOpts = {
-  /**
-   * Skip checking that the directory exists.
-   */
-  lazy?: boolean
-}
-
 export type ContainerExecOpts = {
   /**
    * Command to run instead of the container's default command (e.g., ["run", "main.go"]).
@@ -166,13 +159,6 @@ export type ContainerExportOpts = {
    * engine's cache, then it will be compressed using Gzip.
    */
   forcedCompression?: ImageLayerCompression
-}
-
-export type ContainerFileOpts = {
-  /**
-   * Skip checking that the file exists.
-   */
-  lazy?: boolean
 }
 
 export type ContainerImportOpts = {
@@ -670,6 +656,18 @@ export type ClientGitOpts = {
    * Set to true to keep .git directory.
    */
   keepGitDir?: boolean
+
+  /**
+   * A service which must be started before the repo is fetched.
+   */
+  experimentalServiceHost?: Service
+}
+
+export type ClientHttpOpts = {
+  /**
+   * A service which must be started before the URL is fetched.
+   */
+  experimentalServiceHost?: Service
 }
 
 export type ClientPipelineOpts = {
@@ -829,15 +827,14 @@ export class Container extends BaseClient {
    *
    * Mounts are included.
    * @param path The path of the directory to retrieve (e.g., "./src").
-   * @param opts.lazy Skip checking that the directory exists.
    */
-  directory(path: string, opts?: ContainerDirectoryOpts): Directory {
+  directory(path: string): Directory {
     return new Directory({
       queryTree: [
         ...this._queryTree,
         {
           operation: "directory",
-          args: { path, ...opts },
+          args: { path },
         },
       ],
       host: this.clientHost,
@@ -999,15 +996,14 @@ export class Container extends BaseClient {
    *
    * Mounts are included.
    * @param path The path of the file to retrieve (e.g., "./README.md").
-   * @param opts.lazy Skip checking that the file exists.
    */
-  file(path: string, opts?: ContainerFileOpts): File {
+  file(path: string): File {
     return new File({
       queryTree: [
         ...this._queryTree,
         {
           operation: "file",
-          args: { path, ...opts },
+          args: { path },
         },
       ],
       host: this.clientHost,
@@ -3508,6 +3504,7 @@ export default class Client extends BaseClient {
    * Can be formatted as https://{host}/{owner}/{repo}, git@{host}/{owner}/{repo}
    * Suffix ".git" is optional.
    * @param opts.keepGitDir Set to true to keep .git directory.
+   * @param opts.experimentalServiceHost A service which must be started before the repo is fetched.
    */
   git(url: string, opts?: ClientGitOpts): GitRepository {
     return new GitRepository({
@@ -3542,14 +3539,15 @@ export default class Client extends BaseClient {
   /**
    * Returns a file containing an http remote url content.
    * @param url HTTP url to get the content from (e.g., "https://docs.dagger.io").
+   * @param opts.experimentalServiceHost A service which must be started before the URL is fetched.
    */
-  http(url: string): File {
+  http(url: string, opts?: ClientHttpOpts): File {
     return new File({
       queryTree: [
         ...this._queryTree,
         {
           operation: "http",
-          args: { url },
+          args: { url, ...opts },
         },
       ],
       host: this.clientHost,
