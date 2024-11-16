f9a2e6e39f95868ea5256c7a3b11480ede5e2214
Alex Suraci <alex@dagger.io>
2024-11-16 18:04:01 -0500

    cacheVolume: use dynamic purity
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/schema/cache.go
+++ core/schema/cache.go
@@ -22,8 +22,8 @@ func (s *cacheSchema) Install() {
 	dagql.Fields[*core.Query]{
 		dagql.NodeFunc("cacheVolume", s.cacheVolume).
 			Doc("Constructs a cache volume for a given cache key.").
-			Impure("cacheVolume should be namespaced").
-			ArgDoc("key", `A string identifier to target this cache volume (e.g., "modules-cache").`),
+			ArgDoc("key", `A string identifier to target this cache volume (e.g., "modules-cache").`).
+			ArgDoc("namespace", `An additional key component to namespace the cache volume (e.g., "my-module").`),
 	}.Install(s.srv)
 
 	dagql.Fields[*core.CacheVolume]{}.Install(s.srv)
@@ -34,29 +34,45 @@ func (s *cacheSchema) Dependencies() []SchemaResolvers {
 }
 
 type cacheArgs struct {
-	Key string
+	Key       string
+	Namespace string `default:""`
 }
 
 func (s *cacheSchema) cacheVolume(ctx context.Context, parent dagql.Instance[*core.Query], args cacheArgs) (dagql.Instance[*core.CacheVolume], error) {
 	var inst dagql.Instance[*core.CacheVolume]
 
-	m, err := parent.Self.Server.CurrentModule(ctx)
-	if err != nil && !errors.Is(err, core.ErrNoCurrentModule) {
-		return inst, err
+	if args.Namespace == "" {
+		m, err := parent.Self.Server.CurrentModule(ctx)
+		if err != nil && !errors.Is(err, core.ErrNoCurrentModule) {
+			return inst, err
+		}
+
+		if m != nil {
+			// we're redirecting to a pure value based on the calling module, so
+			// don't cache this call
+			dagql.Taint(ctx)
+
+			err = s.srv.Select(ctx, parent, &inst, dagql.Selector{
+				Field: "cacheVolume",
+				Args: []dagql.NamedInput{
+					{
+						Name:  "key",
+						Value: dagql.NewString(args.Key),
+					},
+					{
+						Name:  "namespace",
+						Value: dagql.NewString(m.Source.ID().Digest().String()),
+					},
+				},
+			})
+			return inst, err
+		}
 	}
 
 	key := args.Key
-	if m != nil {
-		key = m.Source.ID().Digest().String() + "-" + key
+	if args.Namespace != "" {
+		key = args.Namespace + ":" + key
 	}
-
-	return dagql.InstanceWithNewSelector(ctx, s.srv, parent, core.NewCache(key), dagql.Selector{
-		Field: "cacheVolume",
-		Args: []dagql.NamedInput{
-			{
-				Name:  "key",
-				Value: dagql.NewString(key),
-			},
-		},
-	})
+	cache := core.NewCache(key)
+	return dagql.NewInstanceForCurrentID(ctx, s.srv, parent, cache)
 }
