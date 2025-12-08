55743002cd5e87fb1765a457c135987e5423c733
Alex Suraci <alex@dagger.io>
2025-12-08 14:47:27 -0500

    wip(discard): reduce allocs, call store
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/idtui/frontend.go
+++ dagql/idtui/frontend.go
@@ -244,13 +244,13 @@ func (r *renderer) fancyIndent(out TermOutput, row *dagui.TraceRow, selfBar, sel
 		color := restrainedStatusColor(span)
 
 		var prefix string
-		if i == 0 && selfHoriz && !row.Span.Reveal && len(parent.Span.RevealedSpans.Order) == 0 {
+		if i == 0 && selfHoriz && !row.Span.Reveal && len(parent.Span.RevealedSpans().Order) == 0 {
 			if row.Next != nil {
 				prefix = VertRightBar + HorizHalfLeftBar
 			} else {
 				prefix = CornerBottomLeft + HorizHalfLeftBar
 			}
-		} else if nextChild.Next != nil && !row.Span.Reveal && len(parent.Span.RevealedSpans.Order) == 0 {
+		} else if nextChild.Next != nil && !row.Span.Reveal && len(parent.Span.RevealedSpans().Order) == 0 {
 			prefix = VertBar + " "
 		} else {
 			prefix = "  "
